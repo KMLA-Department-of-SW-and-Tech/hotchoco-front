343faf6dd51b66465b4a9f97b8112e4ed57aa448
yujeong <yujeongseok29@gmail.com>
2025-05-27 19:24:39 +0900

    feat(login): create login page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,40 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
+import Login from "./login";
+import { useRouter } from "expo-router";
 
-export default function Index() {
+const Main = ({}) => {
+  const router = useRouter();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Main Page</Text>
+      <TouchableOpacity style={styles.button} onPress={() => router.push("../login")}>
+        <Text style={styles.buttonText}>login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#007AFF",
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+  },
+});
+
+export default Main;
